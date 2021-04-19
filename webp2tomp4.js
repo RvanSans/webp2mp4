c7cc0d5c3eff83b09bffd92586bf20101d26114d
bennysolo <78677744+bennysolo@users.noreply.github.com>
2021-04-20 03:20:37 +0700

    Create webp2tomp4.js

---
+++ webp2tomp4.js
@@ -0,0 +1,50 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+const FormData = require('form-data')
+ 
+ 
+function uploadimg (filename, name) {
+    var image = fs.createReadStream(filename)
+    var form = new FormData()
+    form.append('image', image, name)
+     
+    const upload = fetch('https://storage.naufalhoster.xyz', {
+        method: 'POST',
+        body: form
+    }).then((response) => response.json())
+        .then((result) => {
+            return result
+        })
+        .catch(e => {
+            return e
+        })
+    return upload
+}
+
+
+
+case 'tomp4':
+									
+                                   if (!isQuotedSticker) return reply('Reply stiker nya')
+					if (ben.message.extendedTextMessage.contextInfo.quotedMessage.stickerMessage.isAnimated == true)
+						encmedia = JSON.parse(JSON.stringify(ben).replace('quotedM','m')).message.extendedTextMessage.contextInfo
+						 media = await benny.downloadAndSaveMediaMessage(encmedia)
+						anu = await uploadimg(media, Date.now() + '.webp')
+						console.log(anu)
+						reply(mess.wait)
+						buffer = await getBuffer(`https://api-self.herokuapp.com/api/webp2mp4?img_url=${anu.result.image}`)
+						benny.sendMessage(from, buffer, video, {mimetype: 'video/mp4', caption: 'Nih..'})
+					         break
+		case 'togif':
+									
+                                   if (!isQuotedSticker) return reply('Reply stiker nya')
+					if (ben.message.extendedTextMessage.contextInfo.quotedMessage.stickerMessage.isAnimated == true)
+						encmedia = JSON.parse(JSON.stringify(ben).replace('quotedM','m')).message.extendedTextMessage.contextInfo
+						 media = await benny.downloadAndSaveMediaMessage(encmedia)
+						anu = await uploadimg(media, Date.now() + '.webp')
+						console.log(anu)
+						reply(mess.wait)
+						buffer = await getBuffer(`https://api-self.herokuapp.com/api/webp2mp4?img_url=${anu.result.image}`)
+						benny.sendMessage(from, buffer, video, {mimetype: 'video/gif', caption: 'Nih..'})
+					         break
+		
